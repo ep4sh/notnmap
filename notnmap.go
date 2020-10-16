c89ba3180e08008e62b029dc2077629f9d8cf283
Pasha Radchenko <ep4sh2k@gmail.com>
2020-10-17 00:30:16 +0300

    Rewrite code using channel sync

--- notnmap.go
+++ notnmap.go
@@ -2,52 +2,76 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net"
 	"os"
+	"sync"
 	"time"
 )
 
 const (
-	MAXPORT = 65536
+	MAXPORTS  = 50000 // 1-65535
+	WORKERS   = 100   // 5-100
+	TIMEOUT   = 100   // msec
+	RESULTBUF = 512
 )
 
-func worker(ports, results chan int) {
-	for port := range ports {
-		host := fmt.Sprintf("%s:%d", os.Args[1], port)
-		conn, err := net.DialTimeout("tcp", host, 10*time.Second)
-		if err != nil {
-			results <- 0
-			continue
+func main() {
+	fmt.Println(`
+	 _____  _____  _____  _____  _____  _____  _____
+	 |   | ||     ||_   _||   | ||     ||  _  ||  _  |
+	 | | | ||  |  |  | |  | | | || | | ||     ||   __|
+	 |_|___||_____|  |_|  |_|___||_|_|_||__|__||__|
+
+		`)
+
+	ports := make(chan int, MAXPORTS)
+	results := make(chan int, RESULTBUF)
+	done := make(chan bool)
+	var wg sync.WaitGroup
+
+	// Add ports to channel
+	for i := 1; i <= MAXPORTS; i++ {
+		ports <- i
+	}
+	close(ports)
+
+	// Run workers
+	for i := 1; i <= WORKERS; i++ {
+		wg.Add(1)
+		go func(i int) {
+			wg.Done()
+			worker(i, ports, results, done)
+		}(i)
+	}
+	wg.Wait()
+	for {
+		select {
+		case openPort, ok := <-results:
+			fmt.Println("Found open port:", openPort)
+			if !ok {
+				// ROFL
+				fmt.Println("Err, please restart pc")
+			}
+		case <-done:
+			os.Exit(0)
 		}
-		conn.Close()
-		results <- port
-		return
 	}
 }
 
-func main() {
-	if len(os.Args) >= 2 {
-		ports := make(chan int, MAXPORT)
-		results := make(chan int)
-
-		for i := 0; i <= 1000; i++ {
-			go worker(ports, results)
-		}
-		go func() {
-			for i := 0; i <= cap(ports); i++ {
-				ports <- i
+func worker(id int, ports <-chan int, results chan<- int, done chan<- bool) {
+	for {
+		port, more := <-ports
+		if more {
+			host := fmt.Sprintf("%s:%d", os.Args[1], port)
+			conn, err := net.DialTimeout("tcp", host, TIMEOUT*time.Millisecond)
+			if err != nil {
+				continue
 			}
-		}()
-		for i := 1; i <= MAXPORT; i++ {
-			port := <-results
-			if port != 0 {
-				fmt.Printf("Found open port: %d \n", port)
-			}
+			results <- port
+			conn.Close()
+		} else {
+			done <- true
+			return
 		}
-		close(ports)
-		close(results)
-	} else {
-		log.Fatalf("Specify host to scan")
 	}
 }
